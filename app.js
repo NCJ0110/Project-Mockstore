e5aa9961f1b786b1fdbaebd6ec8b5d8b452ea113
Nick <ncj0110@protonmail.com>
2023-05-03 14:48:58 +0100

    create getProduct method to recieve and deal with data from contentful headless cms

--- app.js
+++ app.js
@@ -3,3 +3,30 @@ const client = contentful.createClient({
     accessToken: '5rR0l33aoKMxUkSBV-VUrTbwb2QY1P-kQSUwepEKHAA'
 })
 
+class Products{
+    async getProducts(){
+        const result = await client.getEntries({
+            content_type: 'mockStore'
+        })
+
+        const {items} = result;
+
+        const products = items.map(item => {
+            const {title, price} = item.fields;
+            const {id} = item.sys;
+            const image = item.fields.image.fields.file.url;
+
+            return {title, price, id, image};
+        })
+
+        return products;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const products = new Products();
+    
+    products.getProducts().then(products => {
+        
+    });
+})
