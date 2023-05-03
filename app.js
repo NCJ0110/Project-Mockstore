505b106e245b761b1638288df04bf7ccfb6449db
Nick <ncj0110@protonmail.com>
2023-05-03 17:52:44 +0100

    create ui class and method to display the data in the DOM

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const client = contentful.createClient({
     accessToken: '5rR0l33aoKMxUkSBV-VUrTbwb2QY1P-kQSUwepEKHAA'
 })
 
+//variables
+const productsCenter = document.querySelector('.products-center');
+
 class Products{
     async getProducts(){
         const result = await client.getEntries({
@@ -23,10 +26,35 @@ class Products{
     }
 }
 
+class UI {
+    displayProducts(products){
+        let productsHTML = ''
+        
+        products.forEach(product => {
+            productsHTML += `
+            <article class="product">
+                <div class="img-container">
+                    <img src="${product.image}" alt="" class="product-img">
+                    <button class="add-cart-btn" data-id="${product.id}">
+                        <i class="fa fa-shopping-cart"></i>
+                        add to cart</button>
+                </div>
+                <h3 class="product-title">${product.title}</h3>
+                <h4 class="product-price">${product.price}</h4>
+            </article>
+            `
+        })
+
+        productsCenter.innerHTML = productsHTML;
+    }
+
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const products = new Products();
+    const ui = new UI(); 
     
     products.getProducts().then(products => {
-        
+        ui.displayProducts(products);
     });
 })
